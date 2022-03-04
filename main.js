2d8d75a62ef8b325f7fec563cf497663085c6857
AAAARRRRRSSSSEEEENNNNYY <90505030+AAAARRRRRSSSSEEEENNNNYY@users.noreply.github.com>
2022-03-04 23:03:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+let scores = 0;
+
+let q1 = prompt("Самый быстрый стиль плавания: a) На спине b) Кроль c) Баттерфляй");
+if (q1 == 'b' || q1 == 'b') {
+    scores = scores + 2 
+    // scores += 2
+} else {
+    scores -= 2
+}
+
+alert(` ${scores} - результат `)
+
+let q2 = prompt("Самый медленый стиль плавания: a) Брасс b) На спине c) Баттерфляй");
+if (q2 == 'a' || q2 == 'а') {
+    scores = scores + 2 
+    // scores += 2
+} else {
+    scores -= 2
+}
+
+
+alert(` ${scores} - результат `)
+
+let q3 = prompt("Каким стилем плавают НЕ на животе: a) Брасс b) На спине c) Баттерфляй");
+if (q3 == 'b' || q3 == 'b') {
+    scores = scores + 2 
+    // scores += 2
+} else {
+    scores -= 2
+}
+
+alert(` ${scores} - результат `)
+
+let q4 = prompt("Какой самый сложный стиль плавания: a) Брасс b) На спине c) Баттерфляй");
+if (q4 == 'c' || q4 == 'c') {
+    scores = scores + 2 
+    // scores += 2
+} else {
+    scores -= 2
+}
+
+
+alert(` ${scores} - результат `)
+ 
+let q5 = prompt("Какой самый распостранённый стиль плавания: a) Кроль b) На спине c) Баттерфляй");
+if (q5 == 'a' || q5 == 'a') {
+    scores = scores + 2 
+    // scores += 2
+} else {
+    scores -= 2
+}
+
+
+alert(` ${scores} - результат `)
